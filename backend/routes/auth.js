2c48fbac6c0e3a10c87e073ba819a6d1263c4252
yusuf atesoglu <93738891+YusufAtesoglu@users.noreply.github.com>
2024-09-24 18:02:13 +0300

    Create token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User.js");
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 // Kullanıcı Oluşturma (Create - Register)
 router.post("/register", async (req, res) => {
   try {
@@ -26,4 +27,69 @@ router.post("/register", async (req, res) => {
     res.status(500).json({ error: "Server error." });
   }
 });
+
+// Kullanıcı girişi (Login)
+const JWT_SECRET = '12345';
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    
+    if (!user) {
+      return res.status(401).json({ error: "Invalid email." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid password." });
+    }
+
+    // Kullanıcı verilerini içeren bir token oluştur
+    const token = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+        username: user.username,
+      },
+      JWT_SECRET,  // Gizli anahtar
+      { expiresIn: '1h' }  // Token geçerlilik süresi
+    );
+
+    res.status(200).json({
+      token, // Token'ı döndür
+      message: 'Login successful',
+    });
+ 
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+  // Tüm kullanıcıları getirme (Read - All)
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+// Belirli bir kullanıcı getirme (Read - Single)
+router.get("/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
 module.exports = router;
