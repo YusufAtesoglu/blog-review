df094a250f01bcdca1d7c5c77d70008e8d1da7e2
yusuf atesoglu <93738891+YusufAtesoglu@users.noreply.github.com>
2024-09-26 14:23:19 +0300

    Added get and delete review api with token

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const Blog = require("../models/Blog"); // Blog modelini içe aktarın
+const Blog = require("../models/Blog.js"); // Blog modelini içe aktarın
+const Review = require("../models/Blog.js");
+const verifyToken = require("../middleware/authMiddleware.js");
 const mongoose = require("mongoose");
 
-// Yorum ekleme endpoint'i
-router.post("/blogs/:blogId/reviews", async (req, res) => {
+// Yorum ekleme api si
+router.post("/blogs/:blogId/reviews",verifyToken, async (req, res) => {
   const { blogId } = req.params; // Blog ID'yi route parametresinden al
   const { text, rating, user } = req.body; // userId yerine user alıyoruz
-
-  
 
   try {
     // Blog'u ID ile bul
@@ -37,4 +37,55 @@ router.post("/blogs/:blogId/reviews", async (req, res) => {
   }
 });
 
+
+
+  // Belirli bir blog un yorumlarını listeleme getirme (Read - Single)
+  router.get("/blogs/:blogId/reviews", async (req, res) => {
+    try {
+      const blogId = req.params.blogId;
+      const blog = await Blog.findById(blogId);
+      
+      if (!blog) {
+        return res.status(404).json({ error: "User not found." });
+      }
+      res.status(200).json(blog.reviews);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Server error." });
+    }
+  });
+
+  // Yorum silme endpoint'i
+router.delete("/delete/:blogId/reviews/:reviewId",verifyToken, async (req, res) => {
+    const { blogId, reviewId } = req.params; // Route parametrelerinden postId ve commentId'yi alıyoruz
+  
+    try {
+      // Post'u bul
+      const blog = await Blog.findById(blogId);
+  
+      if (!blog) {
+        return res.status(404).json({ message: "Blog gönderisi bulunamadı." });
+      }
+  
+      // Yorumu bul ve sil
+      const commentIndex = blog.reviews.findIndex(
+        (review) => review._id.toString() === reviewId
+      );
+  
+      if (commentIndex === -1) {
+        return res.status(404).json({ message: "Yorum bulunamadı." });
+      }
+  
+      // Yorumu sil
+      blog.reviews.splice(commentIndex, 1);
+  
+      // Blog'u kaydet
+      await blog.save();
+  
+      res.status(200).json({ message: "Yorum başarıyla silindi." });
+    } catch (error) {
+      console.error("Yorum silme sırasında hata:", error);
+      res.status(500).json({ message: "Yorum silinirken bir hata oluştu.", error });
+    }
+  });
 module.exports = router;
